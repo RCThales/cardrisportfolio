6ce49afc1abf8044d0f3e5af2c38c9e1818d123b
Thales Cardoso <canadathales@gmail.com>
2023-09-14 21:19:50 -0400

    Added a few projects for testing purposes.

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -2,13 +2,11 @@
 
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import { motion } from "framer-motion"
-import { fadeInAndSlideDownVariants } from "../../animations/animations"
+import { motion } from "framer-motion";
+import { fadeInAndSlideDownVariants } from "../../animations/animations";
 import "react-tabs/style/react-tabs.css"; // Import the default styles
 
 const VideoPlayer = () => {
-
-
   return (
     <>
       <div className="flex justify-center flex-col items-center font-semibold relative">
@@ -34,18 +32,23 @@ const VideoPlayer = () => {
         variants={fadeInAndSlideDownVariants}
       >
         <Tabs className={"glow rounded-lg"}>
-          <TabList className="flex flex-col md:flex-row lg:flex-row flex-wrap justify-center md:justify-start lg:justify-start hover:cursor-pointe rounded-t-lg gap-3 bg-gray-950">
+          <TabList className="flex flex-col md:flex-row lg:flex-row flex-wrap justify-center md:justify-center lg:justify-center hover:cursor-pointe rounded-t-lg gap-3 bg-gray-950">
             <Tab className="text-[.8rem] flex text-center items-center h-[40px] justify-center lg:text-[1.3rem] md:text-[1rem] sm:text-[1rem] p-[5px] bg-purple-700 bg-opacity-[0.4] cursor-pointer outline-none transition-all rounded md:rounded-none lg:rounded-none sm:rounded-lg md:rounded-t-lg lg:rounded-t-lg">
-              Sound Design
+              Game Sound Design
+            </Tab>
+            {/*}
+            <Tab className="text-[.8rem] flex text-center items-center h-[40px] justify-center lg:text-[1.3rem] md:text-[1rem] sm:text-[1rem] p-[5px] bg-purple-700 bg-opacity-[0.4] cursor-pointer outline-none transition-all rounded md:rounded-none lg:rounded-none sm:rounded-lg md:rounded-t-lg lg:rounded-t-lg">
+              Film Sound Design
             </Tab>
             <Tab className="text-[.8rem] flex text-center items-center h-[40px] justify-center lg:text-[1.3rem] md:text-[1rem] sm:text-[1rem] p-[5px] bg-fuchsia-700 bg-opacity-[0.4] cursor-pointer outline-none transition-all rounded md:rounded-none lg:rounded-none sm:rounded-lg md:rounded-t-lg lg:rounded-t-lg">
-              Composition
+              Game Composition
             </Tab>
-            <Tab className="text-[.8rem] flex text-center items-center h-[40px] justify-center lg:text-[1.3rem] md:text-[1rem] sm:text-[1rem] p-[5px] bg-pink-700 bg-opacity-[0.4] cursor-pointer outline-none transition-all rounded md:rounded-none lg:rounded-none sm:rounded-lg md:rounded-t-lg lg:rounded-t-lg">
-              Full Experience
+            <Tab className="text-[.8rem] flex text-center items-center h-[40px] justify-center lg:text-[1.3rem] md:text-[1rem] sm:text-[1rem] p-[5px] bg-fuchsia-700 bg-opacity-[0.4] cursor-pointer outline-none transition-all rounded md:rounded-none lg:rounded-none sm:rounded-lg md:rounded-t-lg lg:rounded-t-lg">
+              Film Composition
             </Tab>
+          */}
           </TabList>
-
+          {/* DEMO REEL 1 */}
           <motion.div
             initial="hidden"
             animate="visible"
@@ -73,7 +76,8 @@ const VideoPlayer = () => {
               </div>
             </TabPanel>
           </motion.div>
-
+          {/*
+          DEMO REEL 2
           <motion.div
             initial="hidden"
             animate="visible"
@@ -101,7 +105,7 @@ const VideoPlayer = () => {
               </div>
             </TabPanel>
           </motion.div>
-
+          DEMO REEL 3
           <motion.div
             initial="hidden"
             animate="visible"
@@ -129,6 +133,35 @@ const VideoPlayer = () => {
               </div>
             </TabPanel>
           </motion.div>
+           DEMO REEL 4 
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={fadeInAndSlideDownVariants}
+            className="w-[90vw] lg:w-[60vw] m-0 mini_phone_reels"
+          >
+            <TabPanel className={"w-[100%]"}>
+              <div className="px-4 lg:px-10 py-4 border-purple-700 border border-opacity-[0.3] min-w-[100] rounded-b-lg blur-bg">
+                <h3 className="p-4 text-[1rem] text-center font-medium">
+                  COMPLETE DEMO REEL WITH MUSIC AND SOUND DESIGN FOR THE FULL
+                  EXPERIENCE 😎
+                </h3>
+                <video
+                  className="cursor-pointer transition-all hover:scale-[1.01]"
+                  poster="https://firebasestorage.googleapis.com/v0/b/thalescardrisportfol.appspot.com/o/both_poster.jpg?alt=media&token=3a3916f9-c6c7-4046-85d7-10d79cbca99a"
+                  width="1500"
+                  controls
+                >
+                  <source
+                    src="https://firebasestorage.googleapis.com/v0/b/thalescardrisportfol.appspot.com/o/testvideo.mp4?alt=media&token=1e1444ff-7cc7-48b5-98b0-cfc7ec67d433"
+                    type="video/mp4"
+                  />
+                </video>
+                <br />
+              </div>
+            </TabPanel>
+          </motion.div>
+          */}
         </Tabs>
       </motion.div>
     </>
